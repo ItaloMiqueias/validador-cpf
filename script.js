7110c79d9273ba3d08cf78e6ac33cb4e03891b01
Ítalo <sousaitalo.de@gmail.com>
2022-05-24 17:17:27 -0300

    finalizado o desafio

--- script.js
+++ script.js
@@ -8,16 +8,49 @@ function validaCPF (cpf) {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
-        console.log("números do cpf " + numeros);
-        console.log("digito do cpf " + digitos);
+        var soma = 0;
         
-        return true;
+        //validação do meu primeiro digito
+         var soma = 0;
 
+         for(var i=10;i>1;i--){
+             soma += numeros.charAt(10-i)*i;
+         }
+
+         console.log('Soma primeiro digito ' + soma);
+
+         var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+         if(resultado != digitos.charAt(0)){
+             return false;
+         }
+
+         console.log(digitos.toString().charAt(0) + ' é a primeira posição da var digitos');
+        
+        //validação segundo digito
+         soma = 0;
+         numeros = cpf.substring(0,10);
+
+            for(var k=11;k>1;k--){
+                soma += numeros.charAt(11-k)*k;
+            }
+            console.log('Soma segundo digito ' + soma);
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            if(resultado != digitos.charAt(1)){
+                return false;
+            }
+            console.log(digitos.toString().charAt(1) + ' é a segunda posição da var digitos');
+
+            return true;
     }
 }
 
 function validacao() {
     console.log('Iniciando validação CPF');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     var cpf = document.getElementById('cpf_digitado').value;
     
